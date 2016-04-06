835127c1af6801d9e806b4e69963b6eeb689c114
GTimoshaz <gtimoshaz@gmail.com>
2016-04-06 16:32:13 +0300

    Trying to add drawing

--- src/main/java/ru/gtimoshaz/electro/MainActivity.java
+++ src/main/java/ru/gtimoshaz/electro/MainActivity.java
@@ -2,19 +2,23 @@ package ru.gtimoshaz.electro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SurfaceView surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
 
         surfaceView.getHolder().addCallback(new MySurfaceView(surfaceView.getContext()));
 
     }
+
 }
--- src/main/java/ru/gtimoshaz/electro/MySurfaceView.java
+++ src/main/java/ru/gtimoshaz/electro/MySurfaceView.java
@@ -1,7 +1,12 @@
 package ru.gtimoshaz.electro;
 
+import android.app.Notification;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -10,15 +15,43 @@ import android.view.SurfaceView;
  */
 public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
+    SurfaceHolder surfaceHolder;
+    Path path;
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
     public MySurfaceView(Context context) {
         super(context);
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            path = new Path();
+            path.moveTo(event.getX(), event.getY());
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            path.lineTo(event.getX(), event.getY());
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            path.lineTo(event.getX(), event.getY());
+        }
+
+        if (path != null) {
+            Canvas canvas = surfaceHolder.lockCanvas();
+            canvas.drawPath(path, paint);
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
+
+        return true;
+    }
+
+    @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        this.surfaceHolder = surfaceHolder;
         Canvas canvas = surfaceHolder.lockCanvas();
         canvas.drawRGB(255, 255, 255);
         surfaceHolder.unlockCanvasAndPost(canvas);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(3);
+        paint.setColor(Color.WHITE);
     }
 
     @Override
