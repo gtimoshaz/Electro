bb739f079028f189b4fb5e9f802df12982081cff
GTimoshaz <gtimoshaz@gmail.com>
2016-04-07 09:06:49 +0300

    Drawing resistor

--- src/main/java/ru/gtimoshaz/electro/MySurfaceView.java
+++ src/main/java/ru/gtimoshaz/electro/MySurfaceView.java
@@ -11,14 +11,37 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by george on 06.04.16.
  */
 public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
 
-    SurfaceHolder surfaceHolder;
-    Path path;
-    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private SurfaceHolder surfaceHolder;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint paint_red = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint paint_rectInResistor = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private class Resistor {
+        float x1;
+        float y1;
+        float x2;
+        float y2;
+        float R;
+        String name;
+
+        public Resistor(float x1, float y1, float x2, float y2, float r, String name) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            R = r;
+            this.name = name;
+        }
+    }
+    private ArrayList<Resistor> resistors = new ArrayList<>();
+    private float down_x, down_y, last_x, last_y;
 
     public MySurfaceView(Context context) {
         super(context);
@@ -32,7 +55,15 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
         surfaceHolder.unlockCanvasAndPost(canvas);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(3);
-        paint.setColor(Color.WHITE);
+        paint.setColor(Color.BLACK);
+
+        paint_red.setStyle(Paint.Style.STROKE);
+        paint_red.setStrokeWidth(3);
+        paint_red.setColor(Color.RED);
+
+        paint_rectInResistor.setStyle(Paint.Style.FILL);
+        paint_rectInResistor.setStrokeWidth(3);
+        paint_rectInResistor.setColor(Color.WHITE);
     }
 
     @Override
@@ -45,22 +76,51 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
     }
 
-    @Override
-    public boolean onTouch(View view, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            path = new Path();
-            path.moveTo(event.getX(), event.getY());
-        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            path.lineTo(event.getX(), event.getY());
-        } else if (event.getAction() == MotionEvent.ACTION_UP) {
-            path.lineTo(event.getX(), event.getY());
+    private void redraw() {
+        Canvas canvas = surfaceHolder.lockCanvas();
+        canvas.drawRGB(255, 255, 255);
+
+        Iterator<Resistor> iterator = resistors.iterator();
+        Resistor resistor;
+        Path rectInResistor = new Path();
+
+        while (iterator.hasNext()) {
+            resistor = iterator.next();
+            canvas.drawLine(resistor.x1, resistor.y1, resistor.x2, resistor.y2, paint);
+            rectInResistor.moveTo(resistor.x1 + (resistor.x2 - resistor.x1) / 3, resistor.y1 + (resistor.y2 - resistor.y1) / 3);
+            rectInResistor.lineTo(resistor.x2 - (resistor.x2 - resistor.x1) / 3, resistor.y2 - (resistor.y2 - resistor.y1) / 3);
+            canvas.drawPath(rectInResistor, paint_red);
         }
 
-        if (path != null) {
-            Canvas canvas = surfaceHolder.lockCanvas();
-            canvas.drawPath(path, paint);
-            surfaceHolder.unlockCanvasAndPost(canvas);
+
+        canvas.drawLine(down_x, down_y, last_x, last_y, paint_red);
+
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+
+        boolean multi = event.getPointerCount() > 1;
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            down_x = event.getX();
+            down_y = event.getY();
+
+            //for (int i = 0; i < resistors.length; i++) {
+
+            //}
+
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            last_x = event.getX();
+            last_y = event.getY();
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+
+            if ((last_x - down_x) * (last_x - down_x) + (last_y - down_y) * (last_y - down_y) > 400)
+                resistors.add(new Resistor(down_x, down_y, last_x, last_y, 220, "R" + resistors.size()));
         }
+
+        redraw();
 
         return true;
     }
